88e08e6640e8dd1f7e6cdad9a699450956f35ba5
Raul <raulrios@live.com.ar>
2023-08-28 21:59:00 -0300

    cambio

--- prueba-clase.py
+++ prueba-clase.py
@@ -1,1 +1,21 @@
 print("Hola Mundo")
+
+git clone <URL del repositorio>
+proyecto/
+app/
+__init__.py
+database.py
+controllers/
+__init__.py
+customer_controller.py
+product_controller.py
+models/
+__init__.py
+customer.py
+product.py
+routes/
+__init__.py
+customer_routes.py
+product_routes.py
+run.py
+config.py
